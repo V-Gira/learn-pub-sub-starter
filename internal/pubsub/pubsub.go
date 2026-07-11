970414761ae6dd71d27d60b9dd16a64a21dbd532
v-jira <virgile.girardin@gmail.com>
2026-07-11 02:25:27 +0200

    ch03 l01 b

--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -7,20 +7,13 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-// PublishJSON publishes a value as JSON to a specified exchange using a key.
 func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
-	// marshall the val to JSON bytes
-	jsonBytes, err := json.Marshal(val)
+	dat, err := json.Marshal(val)
 	if err != nil {
 		return err
 	}
-	// Use the channel's .PublishWithContext method to publish the message to the exchange with the routing key. Some configurations:
-	//
-	ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{
+	return ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{
 		ContentType: "application/json",
-		Body:        jsonBytes,
+		Body:        dat,
 	})
-
-	return nil
 }
-
